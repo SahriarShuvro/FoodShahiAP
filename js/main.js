858456e17aec821cef450cf4994f0740ceb875e8
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2023-01-15 22:01:38 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,4 +14,54 @@ for (let i = 0; i < btnCheck.length; i++) {
     });
 }
 
-console.log(`hi`);
+{
+    function bonusCheckYes() {
+        let salaryBonusInputYes = document.getElementById("salaryBonusInputYes");
+        let salaryBonusInput = document.getElementById("salaryBonusInput");
+
+        if (salaryBonusInputYes.checked == true) {
+            salaryBonusInput.classList.add('activeYes');
+            salaryBonusInput.classList.remove('activeNo');
+        } else {
+            salaryBonusInput.classList.remove('activeYes');
+
+        }
+    }
+
+    function bonusCheckNo() {
+        let salaryBonusInputNo = document.getElementById("salaryBonusInputNo");
+        let salaryBonusInput = document.getElementById("salaryBonusInput");
+
+        if (salaryBonusInputNo.checked == true) {
+            salaryBonusInput.classList.add('activeNo');
+            salaryBonusInput.classList.remove('activeYes');
+        } else {
+            salaryBonusInput.classList.remove('activeNo');
+        }
+    }
+
+
+
+
+
+    // let salaryBonusCheckYes = document.getElementById("salaryBonusCheckYes");
+    // let salaryBonusCheckNo = document.getElementById("salaryBonusCheckNo");
+
+    // let salaryBonusInput = document.getElementById("salaryBonusInput");
+
+    // function bonusCheckYes() {
+    //     if (salaryBonusCheckYes.checked == true) {
+    //         salaryBonusInput.style.display = "block";
+    //     } else {
+    //         salaryBonusInput.style.display = "none";
+    //     }
+    // }
+
+    // function bonusCheckNo() {
+    //     if (salaryBonusCheckNo.checked == true) {
+    //         salaryBonusInput.style.display = "none";
+    //     }
+    // }
+
+
+}
